6c61d83227ab61484cb97d780c8c78d8f89dc034
Victor Hugo <pavictorhugo@gmail.com>
2024-11-12 10:16:37 -0300

    update versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.thorugoh.notetrainer"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
